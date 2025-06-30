0fb46080bc524428c305f20895d98a32507b3551
mdmditto <marcos.mdmd1995@gmail.com>
2025-06-30 18:09:53 -0500

    Update deepscaler_1.5b_8k.sh

--- scripts/deepscaler/train/deepscaler_1.5b_8k.sh
+++ scripts/deepscaler/train/deepscaler_1.5b_8k.sh
@@ -68,5 +68,8 @@ python3 -m verl.trainer.main_ppo \
     +algorithm.use_ray=true \
     +algorithm.ray_address=auto \
     +algorithm.reward.hedging_beta=0.3 \
+    +trainer.experiment_name=ray-hedge \
+    +data.train_files=/home/mm328/rllm/data/deepscaler_train_1pct.parquet \
+    +trainer.total_epochs=3 \
     +trainer.log_level=DEBUG \
     trainer.total_epochs=30 "${@:1}"

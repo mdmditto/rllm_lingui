35a45cf6334221f4e5ec1dac1c51ed9b2f7f5164
mdmditto <marcos.mdmd1995@gmail.com>
2025-06-30 18:09:06 -0500

    Update deepscaler_1.5b_8k.sh

--- scripts/deepscaler/train/deepscaler_1.5b_8k.sh
+++ scripts/deepscaler/train/deepscaler_1.5b_8k.sh
@@ -65,6 +65,8 @@ python3 -m verl.trainer.main_ppo \
     trainer.save_freq=20 \
     trainer.test_freq=20 \
     trainer.default_hdfs_dir=null \
-    +algorithm.use_ray=false \
+    +algorithm.use_ray=true \
+    +algorithm.ray_address=auto \
+    +algorithm.reward.hedging_beta=0.3 \
     +trainer.log_level=DEBUG \
     trainer.total_epochs=30 "${@:1}"

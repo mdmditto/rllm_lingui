728295bd39c07c8d790e47be711c76eb6e758954
mdmditto <marcos.mdmd1995@gmail.com>
2025-07-02 18:57:01 -0500

    Update main_ppo.py

--- verl/verl/trainer/main_ppo.py
+++ verl/verl/trainer/main_ppo.py
@@ -110,7 +110,7 @@ import hydra
 def main(config):
     if not ray.is_initialized():
         ray.init(
-            address="auto",
+            address=environ.get("RAY_ADDRESS", "auto"),
             runtime_env={'env_vars': dict(os.environ)}
         )
 

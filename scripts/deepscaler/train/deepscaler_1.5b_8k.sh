e5cb1910bec422044fe4e996bc1a8ca3a3651bab
mdmditto <marcos.mdmd1995@gmail.com>
2025-07-07 18:45:07 -0500

    Update deepscaler_1.5b_8k.sh

--- scripts/deepscaler/train/deepscaler_1.5b_8k.sh
+++ scripts/deepscaler/train/deepscaler_1.5b_8k.sh
@@ -5,6 +5,9 @@ set -x
 # Warning: Export VLLM_ATTENTION_BACKEND on every machine before starting Ray cluster.
 # vLLM without XFORMERS will results in CUDA errors.
 export VLLM_ATTENTION_BACKEND=XFORMERS
+export PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True"
+export XLA_PYTHON_CLIENT_ALLOCATOR=platform  # For XLA users
+export CUDA_LAUNCH_BLOCKING=1  # For better error tracing
 
 # Parse command line arguments
 while [[ $# -gt 0 ]]; do
@@ -29,7 +32,7 @@ python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=$HOME/rllm/data/deepscaler_train.parquet \
     data.val_files=$HOME/rllm/data/aime.parquet \
-    data.train_batch_size=64 \
+    data.train_batch_size=32 \
     data.val_batch_size=256 \
     data.max_prompt_length=1024 \
     data.max_response_length=8192 \
@@ -37,21 +40,22 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=16 \
-    actor_rollout_ref.actor.ppo_micro_batch_size=8 \
+    actor_rollout_ref.actor.ppo_micro_batch_size=4 \
     actor_rollout_ref.actor.use_dynamic_bsz=True \
-    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=16384 \
+    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=8192 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
     actor_rollout_ref.actor.ulysses_sequence_parallel_size=1 \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    ++actor_rollout_ref.model.use_flash_attention_2=True \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     ++actor_rollout_ref.actor.pipeline_parallel_size=1 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.temperature=0.6 \
     actor_rollout_ref.rollout.val_temperature=0.6 \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.85 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.75 \
     actor_rollout_ref.rollout.n=4 \
     actor_rollout_ref.rollout.n_val=4 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \

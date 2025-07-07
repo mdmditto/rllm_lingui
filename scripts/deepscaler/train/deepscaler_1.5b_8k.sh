ba5aa461af9f2cbb1a736ef5813e0c2fc05b3e9c
mdmditto <marcos.mdmd1995@gmail.com>
2025-07-07 18:12:01 -0500

    Update deepscaler_1.5b_8k.sh

--- scripts/deepscaler/train/deepscaler_1.5b_8k.sh
+++ scripts/deepscaler/train/deepscaler_1.5b_8k.sh
@@ -63,8 +63,8 @@ python3 -m verl.trainer.main_ppo \
     +trainer.val_before_train=True \
     ++algorithm.ray_runtime_env.env_vars.WANDB_API_KEY="${WANDB_API_KEY}" \
     ++algorithm.ray_runtime_env.env_vars.WANDB_ENTITY="${WANDB_ENTITY}" \
-    trainer.n_gpus_per_node=3 \
-    ++trainer.num_rollout_actors=3 \
+    trainer.n_gpus_per_node=2 \
+    ++trainer.num_rollout_actors=2 \
     trainer.nnodes=1 \
     trainer.save_freq=20 \
     trainer.test_freq=20 \
